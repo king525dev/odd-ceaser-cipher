5c38ec1580797386f9e0c18d077af8092a5cdb19
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2024-02-04 13:12:46 +0000

    Created Ascii Character Generator

--- asciiCharacters.js
+++ asciiCharacters.js
@@ -1,23 +1,36 @@
+const print = require('./printer');
 const asciiArray = [];
+const unWantedChars = [
+     0, 7, 8, 9, 10, 11, 12, 13, 14, 15, 127, 27, 32
+]
 
 for (let i = 0; i < 128; i++) {
 
      const conditions = [
-          String.fromCharCode(i) === " ", 
-          typeof(String.fromCharCode(i)), 
-          String.fromCharCode(i) === "&", 
-     ]
+          String.fromCharCode(i) !== " ", 
+          typeof(String.fromCharCode(i)) == "string", 
+          String.fromCharCode(i) !== "&",
+          String.fromCharCode(i).startsWith("\\") == false,
+          String.fromCharCode(i).startsWith("\x00") == false,
+          unWantedChars.includes(i) == false
+          ]
 
-     let deter = 0;
-     for(let j = 0; j < conditions.length; i++){
+     var deter = 0;
+     for(let j = 0; j < conditions.length; j++){
           if(conditions[j]){
                deter++
           }
      }
 
+     // console.log(String.fromCharCode(i) + " -----> " + i)
+     // console.log(unWantedChars.includes(i))
+
      if(deter == conditions.length){
           asciiArray.push(String.fromCharCode(i));
+          console.log(String.fromCharCode(i))
      }
 }
 
-console.log(asciiArray);
+
+console.log(asciiArray)
+print(asciiArray);
--- printer.js
+++ printer.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 
 function printer(string){
-     
+     const content = string.join(" - ");
+     fs.writeFile("print.txt", content, (err) => {
+          if(err){
+               console.log(err);
+          }else{
+               console.log("File Created");
+          }
+     })
 }
+
+module.exports = printer;
