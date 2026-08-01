b4567c28cab0b0492c82691110fcd87861e38dd1
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-08-01 15:48:49 +0100

    Update test.js

--- modules/test.js
+++ modules/test.js
@@ -4,8 +4,8 @@
 // ------------------------------------------------------------
 //  Import your cipher functions
 // ------------------------------------------------------------
-const enV2 = require('./main/encrypt/encrypt');
-const deV2 = require('./main/decrypt/decrypt');
+const enV2 = require('../main/encrypt/encrypt');
+const deV2 = require('../main/decrypt/decrypt');
 
 const encrypt = (string, key) => enV2(string, key);
 const decrypt = (string, key) => deV2(string, key);
@@ -443,9 +443,9 @@ for (const section of sections) {
 console.log(`\n${BOLD}Overall Result: ${totalPassed} passed, ${totalFailed} failed${RESET}`);
 // process.exit(totalFailed > 0 ? 1 : 0);
 
-const { deriveKeys } = require('./main/encrypt/kdf');
-const { encryptWithBlockCipher } = require('./main/encrypt/blockCipher');
-const { decryptWithBlockCipher } = require('./main/decrypt/blockCipherRev');
+const { deriveKeys } = require('../main/encrypt/kdf');
+const { encryptWithBlockCipher } = require('../main/encrypt/blockCipher');
+const { decryptWithBlockCipher } = require('../main/decrypt/blockCipherRev');
 
 const keys = deriveKeys('1234');
 const plain = "hello world";
